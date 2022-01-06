26a64a373353f496d2efac07a165d0bcef8bea6d
Josh Cheney <joshcheney125@gmail.com>
2022-01-06 18:22:15 +0000

    Stuck

--- src/01-count-major-versions-above-10/index.js
+++ src/01-count-major-versions-above-10/index.js
@@ -28,9 +28,30 @@ The results should have this structure:
  *  the number of packages that have a MAJOR semver version 
  *  greater than 10.x.x
  */
+const fetch = require('node-fetch');
 
 module.exports = async function countMajorVersionsAbove10() {
   // TODO
+
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "url": "https://api.npms.io/v2/search/suggestions?q=react",
+      "method": "GET",
+      "return_payload": true
+    })
+  }
+
+  const res = await fetch(`http://ambush-api.inyourarea.co.uk/ambush/intercept`, fetchOptions,)
+
+  const data = await res.json()
+  console.log({ data: data.content })
+
+  const dataContent = data.content
+  console.log(dataContent.package)
 
   return count
 };
